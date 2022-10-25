2622900f893f23d6c7a1631b2436a9a88cf5db0c
Elias Knodel <elias.knodel@gmail.com>
2022-10-25 23:29:52 +0200

    feat: add some basic styling

--- src/lib/components/Theme/Switcher.svelte
+++ src/lib/components/Theme/Switcher.svelte
@@ -2,16 +2,16 @@
     import theme from '$lib/stores/themes';
     let selected = $theme;
 
-    function handleClick() {
+    function switchTheme() {
         theme.set(String(selected));
         document.body.className = String(selected);
     }
 </script>
 
 <select
-    class="bg-blue font-semibold rounded-lg text-sm px-4 py-2.5 text-crust text-center inline-flex items-center"
+    class="bg-blue font-semibold rounded-lg text-sm px-4 py-2.5 text-crust max-h-10"
     bind:value={selected}
-    on:change={() => handleClick()}
+    on:change={() => switchTheme()}
 >
     <option value="">Dark</option>
     <option value="light">Light</option>
